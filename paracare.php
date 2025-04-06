8dcad48ca0b84869b368ec81175c1090188f4860
siwarr15 <138624507+siwarr15@users.noreply.github.com>
2025-04-06 14:01:52 +0200

    Add files via upload

---
+++ paracare.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Paracare - Ma boutique</title>
+</head>
+<body>
+
+  <h1>Bienvenue sur la boutique Paracare</h1>
+
+  <!-- Intégration de la boutique Ecwid -->
+  <div id="my-store-113378202"></div>
+  <script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?113378202&data_platform=code&data_date=2025-04-05" charset="utf-8"></script>
+  <script type="text/javascript">
+    xProductBrowser(
+      "categoriesPerRow=3",
+      "views=grid(20,3) list(60) table(60)",
+      "categoryView=grid",
+      "searchView=list",
+      "id=my-store-113378202"
+    );
+  </script>
+
+</body>
+</html>
